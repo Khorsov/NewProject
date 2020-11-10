eb706d350e5a193b34161f359ae14ac512a80a08
Khorsov <hhorsovv@gmail.com>
2020-11-10 13:21:43 -0500

    Hi

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         //B21 is good at Intellij
+        System.out.println("Hi");
 	// write your code here
     }
 }
